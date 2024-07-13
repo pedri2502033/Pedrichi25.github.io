5904ae2cc3586f5111f936cc8fd2fc995b59a316
pedri2502033 <166783074+pedri2502033@users.noreply.github.com>
2024-07-13 02:25:52 +0000

    Archivo de prueba

--- prueba.php
+++ prueba.php
@@ -1,1 +1,17 @@
-<?php echo 'Hola, mundo'; ?>
+<?php
+header('Content-Type: application/json');
+
+$numeros = array();
+for ($i = 1; $i <= 10; $i++) {
+    $numeros[] = $i;
+    
+}
+
+$numeros2 = array();
+for ($p = 10; $p >= 1; $p--){
+    $numeros2[] = $p;
+}
+
+echo json_encode($numeros);
+echo json_encode($numeros2);
+?>
